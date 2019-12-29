76bf0b5992e4fe36a406874acbf1e65dbeaf404d
katsukawagami <joelai1988@gmail.com>
2019-12-29 17:27:06 +0800

    練習用dir

---
+++ php/temp/tempweb.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="text/javascript" src="C:\Git\practice\javascript\jquery\js.js"></script>
+</head>
+
+<body>
+    <div class="">
+        <div class="">
+            <div class="">123</div>
+        </div>
+    </div>
+</body>
+
+</html>

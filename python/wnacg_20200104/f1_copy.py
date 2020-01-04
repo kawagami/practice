7ea2d70f1b7f01444bde0aac1b7ccc41199bb6bd
katsukawagami <joelai1988@gmail.com>
2020-01-04 23:23:25 +0800

    優化流程

---
+++ python/wnacg_20200104/f1_copy.py
@@ -0,0 +1,35 @@
+
+
+import pyperclip
+
+
+def copy_website():
+    # 紀錄複製的網址
+    # pyperclip.paste()
+    # 紀錄網址的列表
+    copy_list = list()
+
+    if pyperclip.paste() not in copy_list:
+        copy_list.append(pyperclip.paste())
+
+
+def show_website_file_name():
+    # 顯示在copy_list的檔案名稱
+    a = 123
+
+
+def path1_check_direct_download():
+    # 檢查能否直接下載
+    b = 123
+
+def path1_direct_download():
+    # 直接下載
+    b = 123
+
+def path2_crawler_onebyone():
+    # 抓取網頁顯示的每一頁圖片資訊
+    b = 123
+
+def path2_download_onebyone():
+    # 抓取網頁顯示的每一頁圖片資訊
+    b = 123

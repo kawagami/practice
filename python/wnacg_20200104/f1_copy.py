666e42215c233c0ef2af64c362a7b31659a3cab3
katsukawagami <joelai1988@gmail.com>
2020-01-10 21:29:55 +0800

    修改f1架構

--- python/wnacg_20200104/f1_copy.py
+++ python/wnacg_20200104/f1_copy.py
@@ -1,35 +1,62 @@
 
 
 import pyperclip
+import datetime
+import time
 
 
-def copy_website():
+def f1_copy_website(copy_list):
+    """預計設定成無限迴圈但是想不出好的break條件"""
+    """目前先設計成跑10秒"""
     # 紀錄複製的網址
     # pyperclip.paste()
     # 紀錄網址的列表
-    copy_list = list()
+    print("開始複製網址")
+    # 取得現在時間的變數
+    time_start = datetime.datetime.now()
+    # 取得區間為10秒的變數
+    ten_second = datetime.timedelta(seconds=10)
+    # 取得10秒後時間的變數
+    ten_second_after = time_start+ten_second
+    # 當現在的時間小於函數開始後10秒的時候就會繼續while迴圈
+    # 白話一點就是會運行10秒
+    while datetime.datetime.now() < ten_second_after:
+        if pyperclip.paste() in copy_list:
+            pass
+        else:
+            copy_list.append(pyperclip.paste())
+            print(pyperclip.paste(), "加入列表")
+        time.sleep(0.2)
+    # 當函數結束時顯示字串
+    print("複製網址結束")
 
-    if pyperclip.paste() not in copy_list:
-        copy_list.append(pyperclip.paste())
+
+# global copy_list
+target_website = list()
+f1_copy_website(target_website)
+print(target_website)
 
 
-def show_website_file_name():
+def f2_show_website_file_name():
     # 顯示在copy_list的檔案名稱
-    a = 123
+    pass
 
 
-def path1_check_direct_download():
+def f3_path1_check_direct_download():
     # 檢查能否直接下載
-    b = 123
+    pass
 
-def path1_direct_download():
+
+def f4_path1_direct_download():
     # 直接下載
-    b = 123
+    pass
 
-def path2_crawler_onebyone():
-    # 抓取網頁顯示的每一頁圖片資訊
-    b = 123
 
-def path2_download_onebyone():
+def f5_path2_crawler_onebyone():
     # 抓取網頁顯示的每一頁圖片資訊
-    b = 123
+    pass
+
+
+def f6_path2_download_onebyone():
+    # 抓取網頁顯示的每一頁圖片資訊
+    pass

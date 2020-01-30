f4f0de8f11e6d0b4945cc6c010f2317314671fc1
katsukawagami <joelai1988@gmail.com>
2020-01-30 18:51:26 +0800

    整理邏輯中

--- python/Successione di Fibonacci/Successione di Fibonacci.py
+++ python/Successione di Fibonacci/Successione di Fibonacci.py
@@ -1,5 +1,20 @@
 #! python3
 
-n = input("請輸入數字\n")
+n = int(input("請輸入數字\n"))
 
-print("您輸入的是", n)
+print("您輸入的n是", n)
+
+Fibonacci = dict()
+Fibonacci[0] = 0
+Fibonacci[1] = 1
+n_time = 0
+# 當n大於1時
+if n > 1:
+    while n_time < n:
+        n_time += 1
+        if n_time == 0 or 1:
+            Fibonacci[n_time] = n_time
+        elif n_time > 1:
+            Fibonacci[n_time] = Fibonacci[n_time-1]+Fibonacci[n_time-2]
+
+print("Fibonacci Number Is", Fibonacci[n])

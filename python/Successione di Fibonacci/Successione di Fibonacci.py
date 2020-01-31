43f67ac32d602d89598481bc42edaddddc9b50bd
katsukawagami <joelai1988@gmail.com>
2020-01-31 11:33:50 +0800

    覺得應該沒錯 但還是錯了 找錯誤中

--- python/Successione di Fibonacci/Successione di Fibonacci.py
+++ python/Successione di Fibonacci/Successione di Fibonacci.py
@@ -1,20 +1,31 @@
 #! python3
 
-n = int(input("請輸入數字\n"))
+n = int(abs(float(input("請輸入正整數\n"))))
 
-print("您輸入的n是", n)
+print("您輸入的n是", n, "\n")
 
 Fibonacci = dict()
 Fibonacci[0] = 0
 Fibonacci[1] = 1
-n_time = 0
-# 當n大於1時
-if n > 1:
-    while n_time < n:
-        n_time += 1
-        if n_time == 0 or 1:
-            Fibonacci[n_time] = n_time
-        elif n_time > 1:
-            Fibonacci[n_time] = Fibonacci[n_time-1]+Fibonacci[n_time-2]
 
-print("Fibonacci Number Is", Fibonacci[n])
+
+def MathFunction(order):
+    for i in range(order):
+        Fibonacci[i+2] = Fibonacci[i]+Fibonacci[i+1]
+        if i+2 == order:
+            print(Fibonacci[i+2])
+            break
+
+
+MathFunction(n)
+
+# n_time = 0
+# # 當n大於1時
+# if n > 1:
+#     while n_time < n:
+#         n_time += 1
+#         if n_time == 0 or 1:
+#             Fibonacci[n_time] = n_time
+#         elif n_time > 1:
+#             Fibonacci[n_time] = Fibonacci[n_time-1]+Fibonacci[n_time-2]
+# print("Fibonacci Number Is", Fibonacci[n])

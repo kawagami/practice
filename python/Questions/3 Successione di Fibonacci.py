e56751c0501a79b940e875517768f11953c6d8e4
katsukawagami <joelai1988@gmail.com>
2020-02-01 00:23:32 +0800

    1/31 進度結束 明天繼續

--- python/Questions/3 Successione di Fibonacci.py
+++ python/Questions/3 Successione di Fibonacci.py
@@ -2,22 +2,17 @@
 
 # 考慮到輸入為負數、浮點數的狀況 所以套上abs()、float()
 n = int(abs(float(input("請輸入正整數\n"))))
-
 print("您輸入的n是", n, "\n")
-
 Fibonacci = dict()
 Fibonacci[0] = 0
 Fibonacci[1] = 1
 
 
 def MathFunction(order):
-    if order == 0:
-        print("序列", order, "的斐波那契數是", Fibonacci[order], ",")
-    elif order == 1:
-        print("序列", order, "的斐波那契數是", Fibonacci[order], ",")
+    if order == 0 or order == 1:
+        print("序列", order, "的斐波那契數是", Fibonacci[order])
     else:
         for i in range(order):
-
             # 因為起始為0 所以左項設為i+2
             Fibonacci[i+2] = Fibonacci[i+1]+Fibonacci[i]
             if i+2 == order:
@@ -28,14 +23,3 @@ def MathFunction(order):
 
 
 MathFunction(n)
-
-# n_time = 0
-# # 當n大於1時
-# if n > 1:
-#     while n_time < n:
-#         n_time += 1
-#         if n_time == 0 or 1:
-#             Fibonacci[n_time] = n_time
-#         elif n_time > 1:
-#             Fibonacci[n_time] = Fibonacci[n_time-1]+Fibonacci[n_time-2]
-# print("Fibonacci Number Is", Fibonacci[n])
